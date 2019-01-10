a859a0aa893ce70fe16a0398e4c7256ebc9cce61
jhatts <jhatts@hotmail.co.uk>
2019-01-10 21:59:06 +0000

    show calculated payments and created error function

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 // listen for submit
-document.querySelector('#loan-form').addEventListener('submit', calculateResults);
+document
+  .querySelector('#loan-form')
+  .addEventListener('submit', calculateResults);
 
 // calculate results function
 function calculateResults(e) {
@@ -15,5 +17,45 @@ function calculateResults(e) {
   const principal = parseFloat(amount.value);
   const calculatedInterest = parseFloat(interest.value) / 100 / 12;
   const calculatedPayments = parseFloat(years.value) * 12;
+
+  // compute monthly payment
+  const x = Math.pow(1 + calculatedInterest, calculatedPayments);
+  const monthly = (principal * x * calculatedInterest) / (x - 1);
+
+  if (isFinite(monthly)) {
+    monthlyPayment.value = monthly.toFixed(2);
+    totalPayment.value = (monthly * calculatedPayments).toFixed(2);
+    totalInterest.value = (monthly * calculatedPayments - principal).toFixed(2);
+  } else {
+    showError('Please check your numbers');
+  }
+
   e.preventDefault();
+}
+
+// show error function
+function showError(error) {
+  // create div
+  const errorDiv = document.createElement('div');
+
+  // select elements
+  const card = document.querySelector('.card');
+  const heading = document.querySelector('.heading');
+
+  // add class to div
+  errorDiv.className = 'alert alert-danger';
+
+  // add text node and append to div
+  errorDiv.appendChild(document.createTextNode(error));
+
+  // Insert error before heading
+  card.insertBefore(errorDiv, heading);
+
+  // clear error after 3 seconds
+  setTimeout(clearError, 3000);
+}
+
+// clear error function
+function clearError() {
+  document.querySelector('.alert').remove();
 }
